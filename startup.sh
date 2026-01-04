cfaa5383a3c7a1d105bbd9d16aa10871952d81a4
Shubham Raj <shubmrj@gmail.com>
2026-01-04 14:10:14 +0530

    dplo

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing dependencies..."
-pip install -r requirement.txt
-
-echo "Starting Flask app..."
-gunicorn --bind=0.0.0.0 --workers 1 --timeout 60 --access-logfile - --error-logfile - application:app
